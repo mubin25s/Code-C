68b7287965e007adeb5f090517434a11db5566f6
Fathum Mubin <fathummubin26@gmail.com>
2023-09-30 12:00:00 +0600

    update

--- Simple Problems/name.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("Hello, my name is Mubin.\n");
-    printf(" It's a simple C program.\n");
-    printf("I hope you find it useful!\n");
-    return 0;
-}
