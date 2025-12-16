e951edb64f41efb66b615860a801a454a98896b2
Fathum Mubin <fathummubin26@gmail.com>
2025-12-16 16:47:54 +0600

    Add files via upload

---
+++ function/ReverseNumber.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int reverseNumber(int n);
+
+int main() {
+    int n;
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+    printf("Reverse of %d is %d\n", n, reverseNumber(n));
+    return 0;
+}
+
+int reverseNumber(int n) {
+    int reverse = 0, remainder;
+    while (n != 0) {
+        remainder = n % 10;
+        reverse = reverse * 10 + remainder;
+        n /= 10;
+    }
+    return reverse;
+}
