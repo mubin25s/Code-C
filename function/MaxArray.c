0b23199b3c2e8f530e6a405bd6ddca0aad788ac8
Fathum Mubin <fathummubin26@gmail.com>
2025-12-16 16:46:54 +0600

    Add files via upload

---
+++ function/MaxArray.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int findMax(int arr[], int n);
+
+int main() {
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    
+    printf("Enter %d integers: ", n);
+    for(int i=0; i<n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Largest element is %d\n", findMax(arr, n));
+    return 0;
+}
+
+int findMax(int arr[], int n) {
+    int max = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max)
+            max = arr[i];
+    }
+    return max;
+}
