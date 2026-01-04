87face3e14e39afd553f772cb15b80a79725efba
Fathum Mubin <fathummubin26@gmail.com>
2026-01-05 00:42:27 +0600

    Add files via upload

---
+++ Diff header/math_operations.h
@@ -0,0 +1,10 @@
+#ifndef MATH_OPERATIONS_H
+#define MATH_OPERATIONS_H
+
+// Function declarations for mathematical operations
+int add(int a, int b);
+int subtract(int a, int b);
+int multiply(int a, int b);
+float divide(int a, int b);
+
+#endif
---
+++ Diff header/string_utils.h
@@ -0,0 +1,10 @@
+#ifndef STRING_UTILS_H
+#define STRING_UTILS_H
+
+// Function declarations for string utility operations
+int string_length(char *str);
+void string_copy(char *dest, char *src);
+int string_compare(char *str1, char *str2);
+void string_reverse(char *str);
+
+#endif
